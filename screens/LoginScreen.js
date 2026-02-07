cffe7a983cecc080004546c97a223357adec5f04
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-02-07 20:30:55 +0430

    login feature added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,13 +3,19 @@ import AuthContent from "../components/Auth/AuthContent";
 import { login } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authContext = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authContext.authenticate(token);
+      console.log(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed!",
