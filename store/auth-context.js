b58545c4609acae782e633c63ab0121bf0480ae3
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-02-07 20:32:17 +0430

    login and logout logic added

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,35 +1,31 @@
+import { createContext, useState } from "react";
 
-
-const AuthContext = createContext({
-    token: '',
-    isAuthenticated: false,
-    authenticate: (token) => { },
-    logout: () => { }
+export const AuthContext = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
 });
 
 function AuthContextProvider({ children }) {
-    const [authToken, setAuthToken] = useState(null);
+  const [authToken, setAuthToken] = useState(null);
 
-    function authenticate(token) {
-        setAuthToken(token);
-        useEffect(() => {
-            AsyncStorage.setItem('token', token);
-        }, [token]);
-    }
+  function authenticate(token) {
+    setAuthToken(token);
+  }
 
-    function logout() {
-        setAuthToken(null);
-        AsyncStorage.removeItem('token');
-    }
+  function logout() {
+    setAuthToken(null);
+  }
 
-    const value = {
-        token: authToken,
-        isAuthenticated: !!authToken,
-        authenticate: authenticate,
-        logout: logout
-    }
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export default AuthContextProvider;
