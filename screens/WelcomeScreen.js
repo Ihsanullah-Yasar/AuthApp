be3c9a7f482d343caab8876b13ed4439b4984622
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-02-12 22:54:54 +0430

    auth token attached with request

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,15 +1,27 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { getWelcomeMessage } from "../util/auth";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
-  const [fetchedMessage, setFetchedMessage] = useState();
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authContent = useContext(AuthContext);
+  const token = authContent.token;
   useEffect(() => {
-    getWelcomeMessage("").then((response) => {
-      console.log(response.data);
-      setFetchedMessage(response.data);
-    });
-  }, []);
+    getWelcomeMessage(token)
+      .then((response) => {
+        console.log(response.data);
+        setFetchedMessage(response.data);
+      })
+      .catch((error) => {
+        console.group("API Error Details");
+        console.error("Error name:", error.name);
+        console.error("Error message:", error.message);
+        console.error("Error response:", error.response);
+        console.error("Error request:", error.request);
+        console.groupEnd();
+      });
+  }, [token]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
